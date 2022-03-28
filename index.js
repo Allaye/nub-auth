0a4911c161a9f73310b779289f0383866f2e3a8c
Allaye <kolade199@gmail.com>
2022-03-28 13:39:05 +0000

    made @types changes

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ var auth = require("basic-auth");
 var compare = require("tsscmp");
 /**
  * @description Middleware to authenticate users
- * @param {*} options - options object with the following properties: users and challenge
- * @returns
+ * @param {IOptions} options - options object with the following properties: users and challenge
+ * @returns {function} - middleware function
  */
 function nubAuth(options) {
     return function (req, res, next) {
         var credentials = auth(req);
-        if (!credentials ||
-            !checkit(options.users, credentials.name, credentials.pass)) {
+        if (!credentials || !checkit(options.users, credentials.name, credentials.pass)) {
             if (options.challenge) {
                 res.statusCode = 401;
                 res.setHeader("WWW-Authenticate", 'Basic realm="example"');
@@ -28,10 +27,10 @@ function nubAuth(options) {
 ;
 /**
  * @description function to check if user credentials are valid
- * @param {*} users - users credentials object to check the provided credentials against
- * @param {*} name - provided username credential through basic-auth
- * @param {*} pass - provided password credential through basic-auth
- * @returns - true if credentials are valid, false otherwise
+ * @param {string} users - user objects that contain the username and password
+ * @param {string} name - username gotten from basic-auth
+ * @param {string} pass - password gotten from basic-auth
+ * @returns {boolean} - true if credentials are valid, false otherwise
  */
 function checkit(users, name, pass) {
     var valid = true;
